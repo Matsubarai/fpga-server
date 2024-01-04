2957ed812c7fdeda8c9b3aaf126078244775bde9
Qianyu Cheng <qianyu.cheng.chn@gmail.com>
2024-01-04 19:00:13 +0800

    Update env_dealloc.sh

--- bin/env_dealloc.sh
+++ bin/env_dealloc.sh
@@ -3,6 +3,7 @@ docker stop $USER-env
 docker rm $USER-env
 if [ -f "/usr/local/etc/timer_id.$USER" ]
 then
+	echo "deallocate env with devices..."
 	atrm `cat  /usr/local/etc/timer_id.$USER`
-	echo "" > /usr/local/etc/timer_id.$USER
+	echo -n "" > /usr/local/etc/timer_id.$USER
 fi
