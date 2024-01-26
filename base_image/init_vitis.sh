d998677086416724624ae5180a29060b4e8b43f0
Qianyu Cheng <qianyu.cheng.chn@gmail.com>
2024-01-26 22:12:45 +0800

    Update init_vitis.sh

--- base_image/init_vitis.sh
+++ base_image/init_vitis.sh
@@ -4,5 +4,5 @@ if [ -f /tools/Xilinx/Vitis/${XILINX_VERSION}/settings64.sh ]; then
 fi
 
 if [ -d /usr/local/MATLAB ]; then
-    export PATH=/usr/local/MATLAB/bin:$PATH
+    export PATH=/usr/local/MATLAB/R2022b/bin:$PATH
 fi
