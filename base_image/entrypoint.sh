f6e990fa21d75832a970039432c74752c9bcc1d9
Qianyu Cheng <qianyu.cheng.chn@gmail.com>
2024-01-26 22:04:31 +0800

    Update entrypoint.sh

--- base_image/entrypoint.sh
+++ base_image/entrypoint.sh
@@ -1,43 +1,78 @@
 #!/bin/bash
+CONTAINER_USER=${HOST_USER:-vitis}
+CONTAINER_UID=${HOST_UID:-1000}
+CONTAINER_GROUP=${HOST_GROUP:-vitis}
+CONTAINER_GID=${HOST_GID:-1000}
+echo "Starting with USER: ${CONTAINER_USER}, UID: ${CONTAINER_UID}, GROUP: ${CONTAINER_GROUP}, GID: ${CONTAINER_GID}"
 
-#wow!
 cleanup () {
+	kill -s SIGTERM $PID_SUB
 	kill -s SIGTERM $!
 	if [ $PORT ];
 	then
 		echo $PORT >> /usr/local/etc/port_pool
 	fi
+	rm /home/${CONTAINER_USER}/.Xauthority /home/${CONTAINER_USER}/.bashrc /home/${CONTAINER_USER}/.profile /home/${CONTAINER_USER}/.bash_logout
+	rm -rf /home/${CONTAINER_USER}/.jupyter
+	rm -rf /data/.snapshot
+	mv /home/${CONTAINER_USER} /data/.snapshot
 	exit 0
 }
 
 trap cleanup SIGINT SIGTERM
 
-if [ $JUPYTER_ENABLE = "true" ];
-then
-	jupyter lab 2>&1 &
-	PID_SUB=$!
+if [ $PORT ]; then
+	echo "c.NotebookApp.port=$PORT" >> /root/.jupyter/jupyter_notebook_config.py
+ 	echo "c.NotebookApp.port=$PORT" >> /etc/skel/.jupyter/jupyter_notebook_config.py
+	echo "c.ServerApp.port=$PORT" >> /root/.jupyter/jupyter_lab_config.py
+ 	echo "c.ServerApp.port=$PORT" >> /etc/skel/.jupyter/jupyter_lab_config.py
 fi
 
-if [ $VNC_ENABLE = "true" ];
-then
-	VNC_IP=$(hostname -i)
-	$NO_VNC_HOME/utils/novnc_proxy --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT 2>&1 &
-	PID_SUB=$!
-	vncserver -kill $DISPLAY \
-		|| rm -rfv /tmp/.X*-lock /tmp/.X11-unix \
-		|| echo "no locks present"
-	vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None > /dev/null 2>&1
+getent passwd ${CONTAINER_USER} 2>&1 > /dev/null
+USER_EXISTS=$?
+getent passwd ${CONTAINER_UID} 2>&1 > /dev/null
+UID_EXISTS=$?
+getent group ${CONTAINER_GROUP} 2>&1 > /dev/null
+GROUP_EXISTS=$?
+getent group ${CONTAINER_GID} 2>&1 > /dev/null
+GID_EXISTS=$?
+
+if [[ ${GROUP_EXISTS} -ne 0 && ${GID_EXISTS} -ne 0 ]]; then
+    groupadd \
+        --gid ${CONTAINER_GID} \
+        ${CONTAINER_GROUP}
 fi
 
-if [ $1 ];
-then
-	$@ 2>&1 &
-	PID_SUB=$!
+if [[ ${USER_EXISTS} -ne 0 && ${UID_EXISTS} -ne 0 ]]; then
+    useradd \
+        --uid ${CONTAINER_UID} \
+        --gid ${CONTAINER_GID} \
+        --create-home \
+        --home-dir /home/${CONTAINER_USER} \
+        --shell /bin/bash \
+        ${CONTAINER_USER}
+    usermod -aG sudo ${CONTAINER_USER}
+    echo ${CONTAINER_USER}:${CONTAINER_USER} | chpasswd
 fi
+
+chown ${CONTAINER_USER} $(tty)
+
+mv /data/.snapshot/* /home/${CONTAINER_USER}
+
+/usr/sbin/gosu "${CONTAINER_USER}" jupyter lab 2>&1 &
+PID_SUB=$!
+
+/usr/sbin/gosu "${CONTAINER_USER}" "$@" &
 
 wait $PID_SUB
+wait $!
 
 if [ $PORT ];
 then
 	echo $PORT >> /usr/local/etc/port_pool
 fi
+
+rm /home/${CONTAINER_USER}/.Xauthority /home/${CONTAINER_USER}/.bashrc /home/${CONTAINER_USER}/.profile /home/${CONTAINER_USER}/.bash_logout
+rm -rf /home/${CONTAINER_USER}/.jupyter
+rm -rf /data/.snapshot
+mv /home/${CONTAINER_USER} /data/.snapshot
