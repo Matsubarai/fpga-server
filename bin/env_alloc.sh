87609bc635056cb63a90269787c0dc04e60e70f8
Qianyu Cheng <qianyu.cheng.chn@gmail.com>
2024-03-06 16:32:09 +0800

    Update env_alloc.sh

--- bin/env_alloc.sh
+++ bin/env_alloc.sh
@@ -3,7 +3,7 @@ XILINX_VERSION="2022.2"
 EXC="true"
 PUB="false"
 REPO="ghcr.io/matsubarai/Vitis-Docker"
-IMG="${REPO}/vitis:${XILINX_VERSION}"
+IMG="${REPO}/vitis:base"
 while getopts ":d:i:v:m:psh" optname
 do
 	case "$optname" in
@@ -27,35 +27,35 @@ do
 			MNT="$MNT --volume $OPTARG" 
 			;;
 		"h")
-			echo "USAGE: env_alloc [-d <DeviceID[,...]=NULL>] [-s (NO_EXCLUSION_FLAG)] [-p (JUPYTER_ENABLE_FLAG)] [-v <Toolchain=2022.2>] [-i <Image=vitis:base>]"
+			echo "USAGE: env_alloc [-d <DEVICE_ID[,...]=NULL>] [-s (NO_EXCLUSION_FLAG)] [-p (JUPYTER_ENABLE_FLAG)] [-v <TOOLCHAIN=2022.2>] [-i <ENV_IMAGE=vitis:base>]"
 			exit 0
 			;;
 		":")
-			echo "No argument value for option -$OPTARG"
+			echo "[ERROR] No argument value for option -$OPTARG"
 			exit 1
 			;;
 		"?")
-			echo "Unknown option $OPTARG"
+			echo "[ERROR] Unknown option $OPTARG"
 			exit 1
 			;;
 		*)
-			echo "Unknown error while processing options"
+			echo "[ERROR] Unknown error while processing options"
 			exit 1
 			;;
 	esac
 done
 if [ $MNT ]
 then
-	echo "custom mount point:$MNT"
+	echo "[INFO] custom mount point:$MNT"
 fi
-DISPLAY_ID=`echo $DISPLAY | cut -d: -f2 | cut -d. -f1`
-CONTAINER_DISPLAY="$USER-env/`xauth list | grep :$DISPLAY_ID | cut -d'/' -f2`"
-FLAGS="--detach --network host --name $USER-env --hostname $USER-env --workdir /data --runtime=xilinx --env XILINX_VISIBLE_DEVICES=$DEVICE --env XILINX_DEVICE_EXCLUSIVE=$EXC --env XILINX_VERSION=${XILINX_VERSION} --env XILINXD_LICENSE_FILE=/tools/Xilinx --env TZ=Asia/Shanghai --env DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1 --env NO_AT_BRIDGE=1 --env LIBGL_ALWAYS_INDIRECT=1 --env HOST_USER=${USER} --env HOST_UID=$(id -u ${USER}) --env HOST_GROUP=${USER} --env HOST_GID=$(id -g ${USER}) --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --volume /usr/local/MATLAB:/usr/local/MATLAB --volume /tools/Xilinx:/tools/Xilinx --volume /home/$USER:/data --volume /usr/local/etc:/usr/local/etc $MNT"
 
+FLAGS="--detach --network host --name $USER-env --workdir /home/$USER --runtime=xilinx --env XILINX_VISIBLE_DEVICES=$DEVICE --env XILINX_DEVICE_EXCLUSIVE=$EXC --env XILINX_VERSION=${XILINX_VERSION} --env XILINXD_LICENSE_FILE=/tools/Xilinx --env QT_X11_NO_MITSHM=1 --env NO_AT_BRIDGE=1 --env LIBGL_ALWAYS_INDIRECT=1 --env HOST_USER=${USER} --env HOST_UID=$(id -u ${USER}) --env HOST_GROUP=${USER} --env HOST_GID=$(id -g ${USER}) --volume /etc/timezone:/etc/timezone --volume /etc/localtime:/etc/localtime --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --volume /usr/local/MATLAB:/usr/local/MATLAB --volume /tools/Xilinx:/tools/Xilinx --volume /home/$USER:/home/$USER --volume /usr/local/etc:/usr/local/etc $MNT"
+
+docker inspect $USER-env > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-	echo "Environment exists. You can execute or deallocate it."
-	exit 0
+	echo "[WARNING] Environment exists. You can deallocate it later."
+	exec /usr/local/bin/env_exec
 fi
 
 if [ $PUB = "true" ]
@@ -66,18 +66,21 @@ then
 	cat ./.port_pool.temp > /usr/local/etc/port_pool
 	rm ./.port_pool.temp
 
+	echo "[INFO] Jupyter IDE mode"
 	if [ $PORT ]
 	then
-		echo "Publish jupyter port -> http://localhost:$PORT"
+		echo "[INFO] Publish jupyter port $EXPOSE_PORT/tcp -> localhost:$PORT"
 		docker run -p $PORT:$EXPOSE_PORT --env PORT=$PORT $FLAGS $IMG sleep infinity
 	else
-		echo "No valid port for publishing, use a random port"
+		echo "[WARNING] No valid port for publishing, use a random port"
 		docker run -P $FLAGS $IMG sleep infinity
 	fi
 else
-	echo "In command-line mode"
+	echo "[INFO] Command-line mode"
 	docker run $FLAGS $IMG sleep infinity
 fi
+
+sleep 1
 if [ $? -ne 0 ]
 then
 	if [ $PORT ]
@@ -89,8 +92,8 @@ fi
 if [ $DEVICE ]
 then
 	at -f /usr/local/bin/env_dealloc now +2 hours 2>&1 | grep -o '[0-9]\+' | head -1 > /usr/local/etc/timer_id.$USER
-	echo "Device[$DEVICE] will be released after 2 hours."
+	echo "[INFO] Device[$DEVICE] will be released after 2 hours."
 fi
 
-docker exec -it -u $UID $USER-env xauth add $CONTAINER_DISPLAY
-docker exec -it -u $UID $USER-env /bin/bash
+echo "[INFO] !!! sudo password: $USER !!!"
+exec /usr/local/bin/env_exec
